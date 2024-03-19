974e61caf2c208fcc42d39ace33f22c253261c02
Cyberfire <buayagoreng99@gmail.com>
2024-03-19 12:48:53 +0800

    Mmebuat prosedure persegi

---
+++ 154Persegi/154Persegi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+	int sisi;
+
+	void inputdata() {
+		cout << "Masukkan Nilai sisi :";
+		cin >> sisi;
+
+	}
+
+	int hitungluas() {
+		return sisi * sisi;
+	}
+
+	void display() {
+		cout << "Luas Persegi :" << hitungluas() << endl;
+
+	}
+
+	int main() {
+		inputdata();
+		display();
+	}
